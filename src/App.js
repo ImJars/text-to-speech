6e56a1f838cb0114b7829e3f5521dcf6dd00b1a2
ImJars <ra374885@uaeh.edu.mx>
2022-09-09 18:44:05 -0500

    Add Tailwind-Style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 function App() {
   return (
-    <div className="">
-      <h1>Hola Mundo</h1>
+    <div 
+      className="bg-background w-full h-screen grid content-center
+                justify-center"
+    >
+      <div className="bg-bgCard px-10 py-5 text-white">
+        <h1 className="font-bold font-robotoMono text-4xl mb-10
+                      hover:text-buttons hover:scale-110
+                      transition duration-500"
+        >
+          Text to speech
+        </h1>
+        <h2 className="font-robotoMono font-thin text-xs text-buttons mb-1">
+          Texto:
+        </h2>
+        <textarea className="w-80" rows="" cols=""/>
+        <div className="flex justify-between mt-5 font-robotoMono">
+          <button
+            type="button"
+            className="border border-buttons rounded-md 
+                      px-4 py-1 text-buttons hover:bg-opacity-10
+                      hover:bg-buttons transition duration-200
+                      ease-in-out"
+          > 
+            Convert
+          </button>
+          <button
+            type="button"
+            className="border border-buttons rounded-md 
+                      px-4 py-1 text-buttons hover:bg-opacity-10
+                      hover:bg-buttons transition duration-200
+                      ease-in-out"  
+          >
+            Limpiar
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background: '#0A192F',
+        bgCard: '#112240',
+        buttons:'#64FFDA',
+      },
+    },
+    fontFamily:{
+      'robotoMono': ['Roboto Mono', 'monospace'],
+    }
   },
   plugins: [],
 }
